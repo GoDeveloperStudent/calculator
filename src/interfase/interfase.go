f64053c8b413523a96b04c2940a086d4576d62c4
GoDeveloverStudent <emingilyazov@email.com>
2024-05-19 14:45:38 +0500

    начальная форма interfase.go

--- src/interfase/interfase.go
+++ src/interfase/interfase.go
@@ -1,1 +1,14 @@
 package interfase
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+)
+
+func interfase() {
+	a := app.New()
+	w := a.NewWindow("calculator")
+
+	w.SetContent(container.NewWithoutLayout())
+	w.ShowAndRun()
+}
