d1f13759217abfc67e0456e4fc0ee28c9a5cae22
GoDeveloverStudent <emingilyazov@email.com>
2024-05-19 16:46:25 +0500

    добавлен answerRect

--- src/interfase/interfase.go
+++ src/interfase/interfase.go
@@ -1,8 +1,11 @@
 package interfase
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
@@ -15,6 +18,12 @@ func Interfase() {
 	answer := widget.NewLabel("ответ")
 	answer.Move(fyne.NewPos(20, 20))
 
-	w.SetContent(container.NewWithoutLayout(answer))
+	answerRect := canvas.NewRectangle(color.NRGBA{255, 255, 255, 255})
+	answerRect.StrokeColor = color.Black
+	answerRect.StrokeWidth = 1
+	answerRect.Resize(fyne.NewSize(260, 50))
+	answerRect.Move(fyne.NewPos(21, 20))
+
+	w.SetContent(container.NewWithoutLayout(answerRect, answer))
 	w.ShowAndRun()
 }
