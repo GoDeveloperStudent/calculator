69bbc32355b9ade702796275629a896cf02d9100
GoDeveloverStudent <emingilyazov@email.com>
2024-05-19 14:48:37 +0500

    добавленн размер окна

--- src/interfase/interfase.go
+++ src/interfase/interfase.go
@@ -1,6 +1,7 @@
 package interfase
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 )
@@ -8,6 +9,7 @@ import (
 func interfase() {
 	a := app.New()
 	w := a.NewWindow("calculator")
+	w.Resize(fyne.NewSize(300, 356))
 
 	w.SetContent(container.NewWithoutLayout())
 	w.ShowAndRun()
