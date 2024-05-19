4da5bd6d12b31d02723d1da3ab5d8b92e2e7b06f
GoDeveloverStudent <emingilyazov@email.com>
2024-05-19 11:34:38 +0500

    добавленна sub

--- src/math-logic/math-logic.go
+++ src/math-logic/math-logic.go
@@ -8,3 +8,7 @@ func Sum(num1 float32, num2 float32) (answer string) {
 	answer = fmt.Sprintf("%f", num1+num2)
 	return answer
 }
+func Sub(num1 float32, num2 float32) (answer string) {
+	answer = fmt.Sprintf("%f", num1-num2)
+	return answer
+}
