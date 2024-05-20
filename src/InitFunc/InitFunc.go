649d69e320bb53c77c7bb7254b87f481b9f502c7
GoDeveloverStudent <emingilyazov@email.com>
2024-05-20 21:27:14 +0500

    добавленна логика слияния

--- src/InitFunc/InitFunc.go
+++ src/InitFunc/InitFunc.go
@@ -1,1 +1,17 @@
 package initfunc
+
+import mathlogic "GoMod/src/math-logic"
+
+func Initfunc(num1 float32, num2 float32, selected string) (InitAnswer string) {
+	switch selected {
+	case "+":
+		InitAnswer = mathlogic.Sum(num1, num2)
+	case "-":
+		InitAnswer = mathlogic.Sub(num1, num2)
+	case "*":
+		InitAnswer = mathlogic.Div(num1, num2)
+	case "/":
+		InitAnswer = mathlogic.Mul(num1, num2)
+	}
+	return InitAnswer
+}
